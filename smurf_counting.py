ef612584df9dcfdd89b955ec469132e6de9048f0
cgautamdevc14 <48221514+cgautamdevc14@users.noreply.github.com>
2020-03-31 16:50:16 -0700

    Add files via upload

---
+++ smurf_counting.py
@@ -0,0 +1,21 @@
+#----------------------------------------------
+#--- Author         : Gautamdev Chowdary
+#--- Mail           : gchowdar@asu.edu
+#--- Date           : 27th July 2019
+#----------------------------------------------
+
+# Imports
+import tensorflow as tf
+
+# Object detection imports
+from utils import backbone
+from api import object_counting_api
+
+input_video = ".\input_images_and_videos\smurf_input.avi"
+
+# By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+detection_graph, category_index = backbone.set_model('inference_graph', 'labelmap.pbtxt')
+
+is_color_recognition_enabled = 0
+
+object_counting_api.object_counting(input_video, detection_graph, category_index, is_color_recognition_enabled)
